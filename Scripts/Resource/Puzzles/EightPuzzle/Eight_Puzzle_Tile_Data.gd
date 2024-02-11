b6f758b002b6051e96a0cb4e6944c3bc57fffab6
Lázaro Menezes <lazarodm@gmail.com>
2024-02-10 23:25:15 -0300

    Add eight puzzle

---
+++ Scripts/Resource/Puzzles/EightPuzzle/Eight_Puzzle_Tile_Data.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name EightPuzzleTileData
+
+@export var texture: Texture2D
+@export var correct_position: int
--- Scripts/Screen/In_game/Main/main.gd
+++ Scripts/Screen/In_game/Main/main.gd
@@ -7,21 +7,21 @@ enum SCHOOLS {FIRST,SECOND,THIRD}
 
 
 @export var Schools_generation: Array[SCHOOLS] = []
+@export var available_puzzles: Array[PackedScene] = []
 
 @onready var schools_node = $Schools_gui/School_panel/Schools
 @onready var tittle_text = $Gui/Panel/Tittle_text
 @onready var school_panel = $Schools_gui/School_panel
+@onready var puzzle_area = $PuzzleContainer/PuzzleArea/PanelContainer/Container
 
 
 var new_firt_school: PackedScene = preload("res://Scene/Screen/In_game/Schools/school_first.tscn")
 var new_second_school: PackedScene = preload("res://Scene/Screen/In_game/Schools/school_second.tscn")
 var new_third_school: PackedScene = preload("res://Scene/Screen/In_game/Schools/school_third.tscn")
 
-
 func _ready() -> void:
 	_generate_schools()
-	connect("school_selected",selection_school)
-
+	school_selected.connect(selection_school)
 
 func _generate_schools() -> void:
 	for schools_scenes: int in Schools_generation:
@@ -41,15 +41,16 @@ func _generate_schools() -> void:
 	
 	call_deferred("pass_schools",0)
 
-
 func enabled_school(school: Control) -> void:
 	school.disabled = false
-	school.material.set("shader_parameter/Gray",false)
-
+	school.material.set("shader_parameter/Gray", false)
 
 func selection_school(school: Control) -> void:
 	tittle_text.text = school.tittle
-
+	
+	var puzzle: Puzzle = available_puzzles.pick_random().instantiate()
+	puzzle.solved.connect(_on_puzzle_solved)
+	puzzle_area.add_child(puzzle)
 
 func pass_schools(index: int) -> void:
 	var school: Control = schools_node.get_child(index)
@@ -60,8 +61,8 @@ func pass_schools(index: int) -> void:
 	
 	tw.tween_property(school_panel,"position:x",-school.position.x+200,0.3).set_trans(Tween.TRANS_CUBIC)
 
-
-
+func _on_puzzle_solved():
+	call_deferred("queue_free", puzzle_area.get_child(0))
 
 #Debug================================
 var prox = 1
@@ -71,3 +72,4 @@ func _on_pass_button_pressed():
 	
 	if schools_node.get_child_count() <= prox:
 		prox = 0
+#endregion
