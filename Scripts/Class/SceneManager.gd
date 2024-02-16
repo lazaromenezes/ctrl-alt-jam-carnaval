d158384198544efa9cbea0cbc7bd9ce7457515fa
Lazaro Menezes <lazarodm@gmail.com>
2024-02-16 13:42:15 -0300

    Fix issue with file names

--- Scripts/Class/SceneManager.gd
+++ Scripts/Class/SceneManager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var CreditsScreen: PackedScene = preload("res://Scene/Screen/In_game/Credits.tscn")
+var CreditsScreen: PackedScene = preload("res://Scene/Screen/In_game/credits.tscn")
 var GameScreen: PackedScene = preload("res://Scene/Screen/In_game/main.tscn")
 var TitleScreen: PackedScene = preload("res://Scene/Screen/In_game/TitleScreen.tscn")
 var BriefingScreen: PackedScene = preload("res://Scene/Screen/In_game/Briefing/Briefing.tscn")
